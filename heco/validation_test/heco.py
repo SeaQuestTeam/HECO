57ebdad402b8ec51511e44ce9de71e8388164e14
Gianfranco Di Pietro <gianfrancodipietro@gmail.com>
2025-07-03 12:07:28 +0200

    first results in validation test

--- heco/validation_test/heco.py
+++ heco/validation_test/heco.py
@@ -430,6 +430,10 @@ def open_yaml(yaml_file):
         pass #print("Found 'v_o' and 'u_o' variables in dataset") 
     else: 
         print("WARNING: vo and wo varaibles not found in dataset")
+        uo_name = input("Insert the name of the variable used for X component in Stoke drift \n (Stokes drift U) in the dataset: ")
+        vo_name = input("Insert the name of the variable used for Y component in Stoke drift \n (Stokes drift V) in the dataset: ")
+        DS = DS.rename({vo_name: 'vo', uo_name: 'uo'})
+        print("Renaming variable successful") 
 
     return inputdata, DS
 
@@ -480,6 +484,11 @@ def lagrangian_iteration(DS, lat,lon,timedate, D,dt):
 
         lon_new, lat_new = from_xy_to_lonlat(x_new, y_new)
         position = Point(lon_new, lat_new)
+        
+        # DEBUG
+        # print each values for debugging
+        # print(f"particle position at {timedate} - x: {lon_new}, y: {lat_new}, u: {u.values}, v: {v.values}, D: {D}, dt: {dt}")
+
         return position
 
 def get_sim_info(inputdata):
